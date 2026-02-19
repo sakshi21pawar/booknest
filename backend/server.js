9f6481c90b87fcf1b3947d11f45b94f74b4e9e07
sakshi21pawar <sakshipawar21ap@gmail.co>
2026-02-19 23:31:18 +0530

    fix absolute path

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,11 @@ app.use('/api/cart', cartRoutes);
 
 // Serve frontend build for production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../frontend/build')));
+     app.use(express.static('/opt/render/project/src/frontend/build'));
 
-app.get('/{*splat}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
-});
+    app.get('/{*splat}', (req, res) => {
+        res.sendFile('/opt/render/project/src/frontend/build/index.html');
+    });
 }
 
 // Start server

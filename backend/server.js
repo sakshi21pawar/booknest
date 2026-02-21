c8ad71bfc5e09646751f83972e8c56fc7032e98a
sakshi21pawar <sakshipawar21ap@gmail.co>
2026-02-21 10:45:02 +0530

    remove frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ console.log('process.env.JWT_SECRET:', process.env.JWT_SECRET);
 
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://booknest-frontend.onrender.com" // replace with your actual frontend URL after deploying
+}));
 app.use(express.json());
 
 // Test route
@@ -24,15 +25,6 @@ const cartRoutes = require('./routes/cartRoutes');
 app.use('/api/users', userRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/cart', cartRoutes);
-
-// Serve frontend build for production
-if (process.env.NODE_ENV === 'production') {
-     app.use(express.static('/opt/render/project/src/frontend/build'));
-
-    app.get('/{*splat}', (req, res) => {
-        res.sendFile('/opt/render/project/src/frontend/build/index.html');
-    });
-}
 
 // Start server
 const PORT = process.env.PORT || 5000;
